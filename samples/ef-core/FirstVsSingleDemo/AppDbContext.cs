6a5b247cef6f3a9b6c62a5f2c9856494981831ed
邓磊 <denglei1024@foxmail.com>
2026-02-07 10:31:34 +0800

    first vs single demo

--- samples/ef-core/FirstVsSingleDemo/AppDbContext.cs
+++ samples/ef-core/FirstVsSingleDemo/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace FirstVsSingleDemo;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext
+namespace FirstVsSingleDemo;
+
+public class AppDbContext: DbContext
 {
-    
+    public DbSet<User> Users => Set<User>();
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("Data Source=app.db");
+    }
 }
--- samples/ef-core/FirstVsSingleDemo/Program.cs
+++ samples/ef-core/FirstVsSingleDemo/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FirstVsSingleDemo;
+using Microsoft.EntityFrameworkCore;
+
+await using var db = new AppDbContext();
+await db.Database.EnsureDeletedAsync();
+await db.Database.EnsureCreatedAsync();
+
+// init dirty data
+await db.Users.AddRangeAsync(
+    new User { Email = "test@example.com" },
+    new User { Email = "test@example.com" }
+);
+    
+await db.SaveChangesAsync();
+    
+// using FirstOrDefaultAsync
+Console.WriteLine("===== FirstOrDefaultAsync =====");
+var first = await db.Users.FirstOrDefaultAsync(u => u.Email == "test@example.com");
+Console.WriteLine(first is null
+    ? "No User found"
+    : $"user found: {first.Email}");
+
+// using SingleOrDefaultAsync
+try
+{
+    Console.WriteLine("===== SingleOrDefaultAsync =====");
+    var single = await db.Users.SingleOrDefaultAsync(u => u.Email == "test@example.com");
+    Console.WriteLine(single is null
+        ? "No User found"
+        : $"user found: {single.Email}");
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine("Exception thrown:");
+    Console.WriteLine(ex.Message);
+}
+
+
